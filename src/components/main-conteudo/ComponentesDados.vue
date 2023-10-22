9d8bfc0c74c3b7fb08f5ff8a5e207008f818bbff
Chrystian Oliveira <chrystianmoliveira7@gmail.com>
2023-10-22 17:58:07 -0300

    ajuste tamanho area dos componentes de dados

--- src/components/main-conteudo/ComponentesDados.vue
+++ src/components/main-conteudo/ComponentesDados.vue
@@ -1,5 +1,7 @@
 <template>
-  <div class="row-start-2"></div>
+  <div class="flex-column flex h-full flex-wrap justify-center gap-y-2">
+    <div class="flex w-full flex-row gap-2"></div>
+  </div>
 </template>
 
 <script setup lang="ts"></script>
